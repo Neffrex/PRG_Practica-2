30cd0cf09fc581b3a64c0ac7f1bf9b03eb72e8ee
Jialiang <jialiang.chen@estudiants.urv.cat>
2021-11-15 21:36:11 +0100

    perro de tu puta madre

--- src/aplicacio/LlistaEstacionsRecarrega.java
+++ src/aplicacio/LlistaEstacionsRecarrega.java
@@ -1,13 +1,22 @@
 package aplicacio;
 
-import dades.*;
 
+import dades.*;
+/**
+ * 
+ * @author jialiang Chen
+ * @author Jose Luis Pueyo Viltres
+ *
+ */
 public class LlistaEstacionsRecarrega {
 	
 	
 	private EstacioRecarregaVE[] llista;
 	private int nElem;
-	/* 8 */
+	/**
+	 * 8 - registre per saber la distància màxima per poder considerar-se proper
+	 * @param distanciaMaxEstacioPropera - distància màxima que pot estar l'estació com per definir-la com propera
+	 */
 	private static int distanciaMaxEstacioPropera = 30;
 	
 	public LlistaEstacionsRecarrega (int mida) {
@@ -15,109 +24,194 @@ public class LlistaEstacionsRecarrega {
 		nElem = 0;
 	}
 	
-	
-	/*	1
-	 * 	Funció per afegir una estació al final de la llista
-	 * 	Param:
-	 * 		elem - la estació a afegir
-	 * 	Retorna:
-	 * 		boolean - true: s'han afegit l'estació; false: no s'ha pogut afegir l'estació
+	/**
+	 * 1 - Funció per afegir una estació al final de la llista
+	 * @param estacio - la estació a afegir
+	 * @return boolean - true: s'han afegit l'estació; false: no s'ha pogut afegir l'estació
 	 */
-	public boolean afegirEstacio (EstacioRecarregaVE elem) {
+	
+	public boolean afegirEstacio (EstacioRecarregaVE estacio) {
 		if (nElem >= llista.length) return false;
 		
-		llista[nElem++] = elem.copia();
+		llista[nElem++] = estacio.copia();
 		return true;
 	}
 	
+	/**
+	 * 2 - Funció per eliminar totes les estacions d'una població
+	 * @param poblacio - la població de les estacións que es volen eliminar
+	 * @return novaLlista - Llista d'estacions que es troben en aquell municipi
+	 */
 	
-	/*	2
-	 * 	Funció per eliminar totes les estacions d'una població
-	 * 	Param:
-	 * 		poblacio - la població de les estacións que es volen eliminar
-	 * 	Retorna:
-	 * 		boolean - true: s'han eliminat estacions; false: no s'ha eliminat cap estació
+	public LlistaEstacionsRecarrega EstacionsDePoblacioX (String poblacio) {
+		int numPoblacio=0;
+		for (int i=0; i<nElem; i++){
+			if (llista[i].esTrobaEnAquestMunicipi(poblacio)){
+				numPoblacio++;
+			}
+		}
+		LlistaEstacionsRecarrega novaLlista= new LlistaEstacionsRecarrega(numPoblacio);
+		for (int i=0; i<numPoblacio; i++) {
+			if (llista[i].esTrobaEnAquestMunicipi(poblacio)){
+				novaLlista.afegirEstacio(llista[i]);
+			}
+		}
+		return novaLlista;
+	}
+	/**
+	 * 
+	 * @param poblacio - la població de les estacións que es volen eliminar
+	 * @return boolean - true: s'han eliminat les estacions; false: no s'ha pogut fer l'eliminació
 	 */
 	public boolean eliminarEstacionsDePoblacioX (String poblacio) {
-		//TODO eliminarEstacionsDePoblacioX()
-		throw new UnsupportedOperationException("Not Implemented Yet");
-	}
+        int numInicial = nElem;
+        for (int i=0; i<nElem; i++){
+            if (llista[i].esTrobaEnAquestMunicipi(poblacio)){
+                nElem--;
+                for (int j=i; j<nElem; j++){
+                    llista[j]=llista[j+1];
+                }
+            i--;
+            }
+        }
+        return (numInicial>nElem);
+    }
 	
-	
-	/*	3
-	 * 	Funció per obtenir les dades de les estacions d'una població
-	 * 	Param:
-	 * 		poblacio - la població de les estacións de les que vols obtenir les dades
-	 * 	Retorna:
-	 * 		String[] - un vector amb les dades de les estacions, cada element del vector correspon a una estació
+	/**
+	 * 3 - Funció per obtenir les dades de les estacions d'una població
+	 * @param poblacio - la població de les estacións de les que vols obtenir les dades
+	 * @return String[] - un vector amb les dades de les estacions, cada element del vector correspon a una estació
 	 */
+	
 	public String[] dadesEstacionsDePoblacioX (String poblacio) {
-		//TODO dadesEstacionsDePoblacioX()
-		throw new UnsupportedOperationException("Not Implemented Yet");
+		
+		String[] dades;
+		int numDades=0;
+		for (int i=0; i<nElem; i++){
+			if (llista[i].esTrobaEnAquestMunicipi(poblacio)){
+				numDades++;
+			}
+		}
+		dades= new String[numDades];
+
+		for (int i=0, j=0; i<nElem; i++){
+			if (llista[i].esTrobaEnAquestMunicipi(poblacio)){
+				dades[j++] = llista[i].toString();
+			}
+		}
+		return dades;
 	}
 	
-	
-	/*	4
-	 * 	Funció per obtenir la primera estació de la llista que es trobi en la provincia indicada
-	 * 	Param:
-	 * 		provincia - la provincia de la estacio que es cerca
-	 * 	Retorna:
-	 * 		EstacioRecarregaVE - la instància de la primera estació amb la provincia indicada
+	/**
+	 * 4 - Funció per obtenir la primera estació de la llista que es trobi en la provincia indicada
+	 * @param provincia - la provincia de la estacio que es cerca
+	 * @return EstacioRecarregaVE - la instància de la primera estació amb la provincia indicada
 	 */
+	
 	public EstacioRecarregaVE primeraEstacioEnProvinciaX (String provincia) {
-		//TODO primeraEstacioEnProvinciaX()
-		throw new UnsupportedOperationException("Not Implemented Yet");
+		for (int i=0; i<nElem; i++){
+			if (llista[i].esTrobaEnAquestaProvincia(provincia)){
+				return llista[i].copia();
+			}
+		}
+		return null;
 	}
 	
-	
-	/*	5
-	 * 	Funció per obtenir el numero de estacions que disposen de punts de recárrega d'un tipus de velocitat
-	 * 	Param:
-	 * 		velocitat - el tipus de velocitat de les estacions a tenir en compte
-	 * 	Retorna:
-	 * 		int - numero de estacions amb el tipus de velocitat indicada
+	/**
+	 * 5 - Funció per obtenir el numero de estacions que disposen de punts de recárrega d'un tipus de velocitat
+	 * @param velocitat - el tipus de velocitat de les estacions a tenir en compte
+	 * @return int - numero de estacions amb el tipus de velocitat indicada
 	 */
-	public int numEstacionsAmbVelocitatX (int velocitat) {
-		//TODO numEstacionsAmbVelocitatX()
-		throw new UnsupportedOperationException("Not Implemented Yet");
+	
+	public int numEstacionsAmbVelocitatX (String velocitat) {
+		int num=0;
+		for (int i=0; i<nElem;i++){
+			if (llista[i].teAquestTipusRecarrega(velocitat)) num++;
+			// num += llista[i].teAquestTipusRecarrega(velocitat);
+		}
+		return num;
 	}
 	
-	
-	/*	6
-	 * 	Funció per obtenir la estació amb més places de capacitat
-	 * 	Retorna:
-	 * 		EstacioRecarregaVE - la instància de la estació amb mes places de capacitat, en cas d'empat es retorna la primera intancia de les empatades
+	/**
+	 * 6 - Funció per obtenir la estació amb més places de capacitat de la llista
+	 * @return EstacioRecarregaVE - la instància de la estació amb mes places de capacitat, en cas d'empat es retorna la primera intancia de les empatades
 	 */
+	
 	public EstacioRecarregaVE majorNumPlaces () {
-		//TODO majorNumPlaces()
-		throw new UnsupportedOperationException("Not Implemented Yet");
+		EstacioRecarregaVE estacioMesPlaces = llista[0];
+		for (int i=1; i<nElem; i++)
+			if (llista[i].getNumPlaces() > estacioMesPlaces.getNumPlaces())
+				estacioMesPlaces = llista[i];
+		return estacioMesPlaces;
 	}
-	
-	
-	/*	7
-	 * 	Funció per obtenir un duplicat de la estació més propera a una posició
-	 *	Param:
-	 *		posicio - posició de la qual es determina si la estació es propera o no 
-	 * 	Retorna:
-	 * 		EstacioRecarregaVE - la instància de la estació amb mes places de capacitat, en cas d'empat es retorna la primera intancia de les empatades
+
+	/**
+	 * 7 - Funció per obtenir un duplicat de la estació més propera a una posició
+	 * @param latitud - latitud de la posició
+	 * @param longitud - longitud de la posició
+	 * @return EstacioRecarregaVE - la instància de la estació amb mes places de capacitat, en cas d'empat es retorna la primera intancia de les empatades
 	 */
-	public EstacioRecarregaVE estacioMesProperaAPosicioX (float posicio) {
-		//TODO estacioMesPropera()
-		throw new UnsupportedOperationException("Not Implemented Yet");
+	
+	public EstacioRecarregaVE estacioMesProperaAPosicioX (float latitud, float longitud) {
+		
+		EstacioRecarregaVE estacioMesPropera = llista[0];
+		double distanciaMenor = llista[0].distanciaA(latitud, longitud);
+		
+		for (int i=1; i<nElem; i++) {
+			double distancia = llista[i].distanciaA(latitud, longitud);
+			if (distancia < distanciaMenor) {
+				estacioMesPropera = llista[i];
+				distanciaMenor = distancia;
+			}
+		}
+		return estacioMesPropera;
 	}
 	
-	
-	/*	9
-	 * 	Funció per obtenir un vector amb les estacions properes a una posició
-	 *	Param:
-	 *		posicio - posició de la qual es determina si la estació es propera o no 
-	 * 	Retorna:
-	 * 		EstacioRecarregaVE[] - vector amb totes les estacions properes a la posició indicada
+	/**
+	 * 9 - Funció per obtenir un vector amb les estacions properes a una posició
+	 * @param latitud - latitud de la posició
+	 * @param longitud - longitud de la posició
+	 * @return LlistaEstacionsRecarrega - llista amb totes les estacions properes a la posició indicada
 	 */
-	public EstacioRecarregaVE[] estacionsProperesAPosicioX (float posicio) {
-		//TODO estacionsProperesAPosicioX()
-		throw new UnsupportedOperationException("Not Implemented Yet");
+	
+	public LlistaEstacionsRecarrega estacionsProperesAPosicioX (float latitud, float longitud) {
+		LlistaEstacionsRecarrega llistaPropers = new LlistaEstacionsRecarrega(nElem);
+		for (int i=0; i<nElem; i++)
+			if (llista[i].distanciaA(latitud, longitud) < distanciaMaxEstacioPropera)
+				llistaPropers.afegirEstacio(llista[i]);
+		return llistaPropers;
 	}
 	
+	public EstacioRecarregaVE get(int i) {
+		return (i<=nElem && llista[i]!=null)?llista[i]:null;
+	}
+	
+	public boolean esBuida() {
+		return (nElem == 0);
+	}
+	
+	public LlistaEstacionsRecarrega duplicat() {
+		LlistaEstacionsRecarrega duplicat = new LlistaEstacionsRecarrega(llista.length);
+		for (int i = 0; i<this.nElem; i++) 
+			duplicat.afegirEstacio(llista[i]);
+		return duplicat;
+	}
+	
+	@Override
+	public String toString() {
+		String buffer = "LlistaEstacionsRecarrega =>";
+		for(int i=0; i<nElem; i++)
+			buffer += "\n\t"+llista[i].toString();
+		return buffer;
+	}
+	
+
+	public EstacioRecarregaVE[] getLlista() {return llista;}
+	public int getnElem() {return nElem;}
+	public static int getDistanciaMaxEstacioPropera() {return distanciaMaxEstacioPropera;}
+
+	public static void setDistanciaMaxEstacioPropera(int distanciaMaxEstacioPropera) {
+		LlistaEstacionsRecarrega.distanciaMaxEstacioPropera = distanciaMaxEstacioPropera;
+	}
 	
 }
--- src/aplicacio/UsaLlistaEstacionsVE.java
+++ src/aplicacio/UsaLlistaEstacionsVE.java
@@ -3,25 +3,187 @@ package aplicacio;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 import dades.*;
 
 public class UsaLlistaEstacionsVE {
 	static Scanner teclat = new Scanner(System.in);
 
 	public static void main(String[] args) throws FileNotFoundException {
+		/* Declaració de les dades */
+		String buffer;
+		LlistaEstacionsRecarrega llistaEstacionsProperes = null;
+		EstacioRecarregaVE estacionsProperes[] = null;
+		byte opcio=0;
+		boolean end=false;
+
 		System.out.println("Indica el número de línies a llegir del fitxer (màxim 418)");
 		int numLinies = Integer.parseInt(teclat.nextLine());
+		Float latitud, longitud;
 		String[] dataset = llegirLiniesFitxer(numLinies);
 
 		for (int i = 0; i < dataset.length; i++) {
 			System.out.println("Linia " + (i + 1) + " conté " + dataset[i]);
 		}
 
-		// Completar el codi a partir d'aquí
+		LlistaEstacionsRecarrega llista = new LlistaEstacionsRecarrega(dataset.length);
+		System.out.println("Carregant dades...");
+		carregar_dades(llista, dataset);
+		System.out.println("Dades carregades i llestes per usar-se");
+		
+		Scanner teclat = new Scanner(System.in);
+		
+		while (!end){
+			
+			System.out.println("**********************************************************************");
+			System.out.println("Menu de l'aplicació EstacióRecàrregaVE");
+			System.out.println("Escull l'opció que vols usar, per seleccionar-ne un introdueix el número corresponent i pressiona Enter.");
+			System.out.println(" 1- Eliminar conjunt d'estacions en una població.");
+			System.out.println(" 2- Mostrar primera estació ubicada a la província de Lleida i Barcelona.");
+			System.out.println(" 3- Mostrar número d'estacions que disposen de punts de recàrrega d'un tipus de velocitat.");
+			System.out.println(" 4- Mostrar l’estació que té més places de capacitat.");
+			System.out.println(" 5- Mostrar l’estació més propera a la nostra posició.");
+			System.out.println(" 6- Mostrar les dades de les estacions que es troben properes a la nostra posició");
+			System.out.println(" 7- Amplia el valor de distància propera a 50 km i recalcula les estacions properes");
+			System.out.println(" 8- Del resultat de les estacions properes mostrar les dades de l’estació amb més places de capacitat.");
+			System.out.println(" 9- Mostrar el conjunt d’estacions de la llista.");
+			System.out.println("10- Sortir del programa.");
+			System.out.println("**********************************************************************");
+			
+			try {
+				
+				System.out.println("Selecciona una opció: ");
+				opcio = Byte.parseByte(teclat.nextLine());
+				
+				System.out.println("Executant opció " + opcio);
+				
+				switch(opcio){
+					case 1:
+						System.out.println("Poblacio del conjunt a eliminar: ");
+						buffer = teclat.nextLine();
+						System.out.println("La llista a eliminar és:"+(llista.EstacionsDePoblacioX(buffer).toString()));
+						
+						System.out.println("Vols eliminar aquestes estacions de la llista? Introdueix 1 Si vols eliminar-los, 0 en cas contrari.");
+						if (teclat.nextInt()==1){
+							System.out.println((llista.eliminarEstacionsDePoblacioX(buffer)?"Estacions eliminades amb exit":"No s'ha fet cap eliminació"));
+						}
+						else System.out.println("No s'ha eliminat cap estació.");
+						break;
+						
+					case 2:
+						LlistaEstacionsRecarrega llistaLleidaBarcelona= new LlistaEstacionsRecarrega(2);
+						llistaLleidaBarcelona.afegirEstacio(llista.primeraEstacioEnProvinciaX("Lleida"));
+						llistaLleidaBarcelona.afegirEstacio(llista.primeraEstacioEnProvinciaX("Barcelona"));
+						System.out.println(llistaLleidaBarcelona.toString());
+						
+						System.out.println("Introdueix la altitud:");
+						latitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("Intrudueix la longitud:");
+						longitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("Distancia a Lleida: "+llistaLleidaBarcelona.get(0).distanciaA(latitud, longitud));
+						System.out.println("Distancia a Barcelona: "+llistaLleidaBarcelona.get(1).distanciaA(latitud, longitud));
+						System.out.println("Estacio més propera: "+llistaLleidaBarcelona.estacioMesProperaAPosicioX(latitud, longitud).toString());
+						break;
+						
+					case 3:
+						System.out.println("Tipus de velocitat: ");
+						buffer = teclat.nextLine();
+						System.out.println("Hi han " + llista.numEstacionsAmbVelocitatX(buffer) + " estacions amb velocitat " + buffer + ".\n");
+						break;
+						
+					case 4:
+						if (llista==null){
+							System.out.println("La llista està buida.");
+						} else {
+							System.out.println(llista.majorNumPlaces().toString());
+						}
+						break;
+						
+					case 5:
+						System.out.println("Introdueix la latitud:");
+						latitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("Intrudueix la longitud:");
+						longitud=Float.parseFloat(teclat.nextLine());
+						System.out.println(llista.estacioMesProperaAPosicioX(latitud, longitud).toString());
+						break;
+						
+					case 6:
+						System.out.println("Introdueix la latitud:");
+						latitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("Intrudueix la longitud:");
+						longitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("El rang de proximitat actual es: " + LlistaEstacionsRecarrega.getDistanciaMaxEstacioPropera());
+						
+						if (estacionsProperes==null) {
+							System.out.println("No hi ha cap estació propera.");
+							break;
+						}
+						System.out.println(llista.estacionsProperesAPosicioX(latitud, longitud).toString());
+						break;
+						
+					case 7:
+						if(LlistaEstacionsRecarrega.getDistanciaMaxEstacioPropera() != 50) {
+							LlistaEstacionsRecarrega.setDistanciaMaxEstacioPropera(50);
+							System.out.println("Rang augmentat a 50km");
+						}
+						
+						System.out.println("Introdueix la latitud:");
+						latitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("Intrudueix la longitud:");
+						longitud=Float.parseFloat(teclat.nextLine());
+						System.out.println("El rang de proximitat actual es: " + LlistaEstacionsRecarrega.getDistanciaMaxEstacioPropera());
+						
+						llistaEstacionsProperes = llista.estacionsProperesAPosicioX(latitud, longitud);
+						if (llistaEstacionsProperes==null) {
+							System.out.println("No hi ha cap estació propera.");
+							break;
+						}
+						System.out.println(llistaEstacionsProperes.toString());
+						break;
+						
+					case 8:
+						if (llistaEstacionsProperes.esBuida()){
+							System.out.println("No hi ha cap estació propera.");
+						} else {
+							System.out.println(llistaEstacionsProperes.majorNumPlaces().toString()); 
+						}
+						break;
+						
+					case 9:
+						System.out.println(llista.toString());
+						break;
+						
+					case 10:
+						System.out.println("Sortint del programa...");
+						end = true;
+						break;
+						
+					default:
+						System.out.println("Introdueix un valor vàlid.");
+						break;
+				}
 
+				waitForResponse("Prem ENTER per continuar... ");
+				
+			} catch(NumberFormatException e) {
+				// Formato incorrecto al introducir un dato
+				System.err.println("ERROR: El valor introduit no es un valor vàlid");
+				waitForResponse("Prem ENTER per continuar... ");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+
+		}
+		
+		teclat.close();
 		
 	}
-
+	
+	private static void waitForResponse(String msg) {
+		System.out.println(msg); teclat.nextLine();
+	}
+	
 	private static String[] llegirLiniesFitxer(int nLinies) throws FileNotFoundException {
 		String[] result;
 		if (nLinies < 0)
@@ -39,5 +201,30 @@ public class UsaLlistaEstacionsVE {
 		f.close();
 		return result;
 	}
-
+	
+	private static void carregar_dades(LlistaEstacionsRecarrega llistaEstacions, String[] dataset) {
+		
+		for(int i=0; i<dataset.length; i++) {
+			//Separació de les dades
+			String s = dataset[i];
+			s = s.replace(',', '.');
+			String[] dadesEstacio = s.split(";");
+			String[] velocitats = dadesEstacio[1].split(" i ");
+			
+			//Omplir la llista amb les estacions amb les dades corresponents a dataset
+			llistaEstacions.afegirEstacio(new EstacioRecarregaVE(
+					dadesEstacio[0],
+					velocitats[0],
+					Float.parseFloat(dadesEstacio[2]),
+					Float.parseFloat(dadesEstacio[3]),
+					dadesEstacio[4],
+					dadesEstacio[5],
+					Integer.parseInt(dadesEstacio[6])));
+			
+			for (int j=1; j<velocitats.length; j++)
+				llistaEstacions.getLlista()[i].afegirTipusVelocitat(velocitats[j]);
+		}
+		
+	}
+	
 }
